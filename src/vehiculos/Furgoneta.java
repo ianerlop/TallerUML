b1d2c4a61a67372c9471a8749cedcfee22c905b2
David Alarcon Galeas <alarcondavid2497@gmail.com>
2020-11-19 14:00:39 -0500

    clase furgoneta

--- src/vehiculos/Furgoneta.java
+++ src/vehiculos/Furgoneta.java
@@ -1,5 +1,37 @@
 package vehiculos;
 
-public class Furgoneta {
+import java.util.Date;
 
+import empleados.Permanente;
+
+public class Furgoneta extends VehiculoCarga {
+	
+	private double pesoSinCarga;
+	private double capacidadCarga;
+	
+	
+	public Furgoneta(String numRegistro, double capacidadMotor, Date proxFechaPrueba, double pesoCargado,
+			Fabricante fabricante, Permanente asignado, double pesoSinCarga, double capacidadCarga) {
+		super(numRegistro, capacidadMotor, proxFechaPrueba, pesoCargado, fabricante, asignado);
+		this.pesoSinCarga = pesoSinCarga;
+		this.capacidadCarga = capacidadCarga;
+	}
+	
+	public double getPesoSinCarga() {
+		return pesoSinCarga;
+	}
+	
+	public void setPesoSinCarga(double pesoSinCarga) {
+		this.pesoSinCarga = pesoSinCarga;
+	}
+	
+	public double getCapacidadCarga() {
+		return capacidadCarga;
+	}
+	
+	public void setCapacidadCarga(double capacidadCarga) {
+		this.capacidadCarga = capacidadCarga;
+	}
+	
+	
 }
