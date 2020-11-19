8856b4d1f102ddf13c7e5ae172527cd9ce64f5cd
Daniel <dafebust@espol.edu.ec>
2020-11-19 11:28:26 -0700

    Se crea la clase camion que extiende de VehiculoCarga

---
+++ src/vehiculos/Camion.java
@@ -0,0 +1,43 @@
+package vehiculos;
+
+import java.util.Date;
+
+import empleados.Permanente;
+
+public class Camion extends VehiculoCarga{
+	private int numeroRuedas;
+	private double capacidadRemolque;
+	private String licencia;
+
+	public Camion(String numRegistro, double capacidadMotor, Date proxFechaPrueba, double pesoCargado, Fabricante fabricante, Permanente asignado,int numeroRuedas, double capacidadRemolque, String licencia) {
+		super(numRegistro, capacidadMotor, proxFechaPrueba, fabricante,pesoCargado,fabricante,asignado);
+		this.numeroRuedas=numeroRuedas;
+		this.capacidadRemolque=capacidadRemolque;
+		this.licencia=licencia;
+	}
+
+	public int getnumeroRuedas() {
+		return numeroRuedas;
+	}
+	
+	public double getcapacidadRemolque() {
+		return capacidadRemolque;
+	}
+	
+	public String getlicencia() {
+		return licencia;
+	}
+
+	public void setnumeroRuedas(int numeroRuedas) {
+		this.numeroRuedas = numeroRuedas;
+	}
+	
+	public void setcapacidadRemolque(double capacidadRemolque) {
+		this.capacidadRemolque=capacidadRemolque;
+	}
+	
+	public void setlicencia(String licencia) {
+		this.licencia=licencia;
+	}
+		
+}
